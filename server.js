4d3898ad23d254c5b951f4bbaaafca284408b100
Nikola <tipic.n@outlook.com>
2022-02-20 22:07:09 +0100

    addDispenser repair

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("./config/db")
 const cors = require("cors");
 
 const app = require("express")();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const UserRouter = require("./api/User");
 const DispenserRouter = require("./api/Dispenser");
