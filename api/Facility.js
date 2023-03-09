28e6584e3074d89304702175546caeaf91eaaad0
Nikola <tipic.n@outlook.com>
2023-03-09 14:14:27 +0100

    delete Facility added

--- api/Facility.js
+++ api/Facility.js
@@ -59,4 +59,47 @@ router.post("/addFacility", (req, res) => {
     }
 });
 
+
+//deleteFacility
+router.post("/deleteFacility", (req, res) => {
+    let { name } = req.body;
+
+    if ( name == "" ) {
+        res.json({
+            status: "FAILED",
+            message: "Morate navesti ime objekta kojeg zelite obrisati!"
+        })
+    } else {
+        name = name.toUpperCase();
+        
+        Facility.find({ name }).then(result => {
+            if(result.length) {
+                Facility.deleteOne({ name }).then(delResult => {
+                    res.json({
+                        status: "SUCCESS",
+                        message: `Objekt ${name} je uspiješno Obrisan`,
+                        data: delResult
+                    });
+                }).catch(err => {
+                    res.json({
+                        status: "FAILED",
+                        message: "An error occured while trying to delete Facility!",
+                    })
+                })
+            } else {
+                res.json({
+                    status: "FAILED",
+                    message: `Objekt s imenom: ${name}, ne postoji u vasoj bazi podataka!`
+                });
+            }
+        }).catch(err => {
+            res.json({
+                status: "fAILED",
+                message: "An error occured while trying to find Facility to delete it!"
+            })
+        })
+    }
+});
+
+
 module.exports = router;
