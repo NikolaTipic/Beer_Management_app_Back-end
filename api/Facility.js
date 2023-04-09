23c9d7d564a7df079f38fe2b3135805736b460c4
Nikola <tipic.n@outlook.com>
2023-04-09 21:39:30 +0200

    response modified in addDatetoDispenser in Faclity.js

--- api/Facility.js
+++ api/Facility.js
@@ -392,7 +392,7 @@ router.post("/addDateOfLastSanitation", (req, res) => {
 
         res.json({
             status: "FAILED",
-            message: `Točionik pod inventurnim brojem: ${invNumber}, ne pripada ni jedom objektu pod vašom nadležnosti!`
+            message: `Točionik pod inventurnim brojem: ${invNumber}, ne pripada ni jedom objektu!`
         });
     });
 });
